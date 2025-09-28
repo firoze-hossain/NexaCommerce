e33a226a37efaf2e20c6d86e42a4d4c7191e74bc
firoze-hossain <firoze.hossain@outlook.com>
2025-09-28 11:54:23 +0600

    first commit

---
+++ src/main/java/com/roze/nexacommerce/NexaCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.roze.nexacommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexaCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexaCommerceApplication.class, args);
+	}
+
+}
