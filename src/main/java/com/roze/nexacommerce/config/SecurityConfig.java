ad6f3c02f621e0c35311e2ffbcabf7c22e590b39
firoze-hossain <firoze.hossain@outlook.com>
2025-10-08 20:28:46 +0600

    security bypass for public api

--- src/main/java/com/roze/nexacommerce/config/SecurityConfig.java
+++ src/main/java/com/roze/nexacommerce/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.roze.nexacommerce.security.JwtAuthFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -48,6 +49,12 @@ public class SecurityConfig implements WebMvcConfigurer {
                 .authorizeHttpRequests(
                         auth -> auth.requestMatchers("/auth/**")
                                 .permitAll()
+                                .requestMatchers(HttpMethod.GET, "/categories").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/products").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/categories/**").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/products/**").permitAll()
+                                .requestMatchers(HttpMethod.GET,"/brands").permitAll()
+                                .requestMatchers(HttpMethod.GET,"/brands/**").permitAll()
                                 .requestMatchers(
                                         "/public/**",
                                         "/customers/register",
